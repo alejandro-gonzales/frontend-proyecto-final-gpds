b185025ef78cdfa8bd4aa791a27e9cd77dcf3724
Alejandro Gonzales Viera <alejandro.g280211@gmail.com>
2023-06-27 04:29:54 -0400

    CRUD Prestamo Terminado

--- src/app/servicios-backend/prestamo/prestamo.service.ts
+++ src/app/servicios-backend/prestamo/prestamo.service.ts
@@ -1,9 +1,72 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PrestamoService {
 
-  constructor() { }
+  PATH_BACKEND = environment.URL_BACKEND + environment.PORT_BACKEND;
+
+  URL_GET_PRESTAMO = this.PATH_BACKEND + "/api/Prestamo"
+  URL_GET_BY_ID_PRESTAMO = this.PATH_BACKEND + "/api/Prestamo/GetPrestamoById"
+  URL_ADD_PRESTAMO = this.PATH_BACKEND + "/api/Prestamo/AddPrestamo"
+  URL_UPDATE_PRESTAMO = this.PATH_BACKEND + "/api/Prestamo/UpdatePrestamo"
+  URL_DELETE_PRESTAMO = this.PATH_BACKEND + "/api/Prestamo/DeletePrestamo"
+  URL_GET_USUARIO = this.PATH_BACKEND + "/api/Usuario"
+  URL_GET_LIBRO = this.PATH_BACKEND + "/api/Libro"
+
+  constructor(private http: HttpClient) { }
+
+  public GetPrestamo(): Observable<HttpResponse<any>> {
+
+    return this.http
+      .get<any>(this.URL_GET_PRESTAMO,
+        { observe: 'response' })
+      .pipe();
+  }
+
+  public GetUsuario(): Observable<HttpResponse<any>> {
+
+    return this.http
+      .get<any>(this.URL_GET_USUARIO,
+        { observe: 'response' })
+      .pipe();
+  }
+
+  public GetLibro(): Observable<HttpResponse<any>> {
+
+    return this.http
+      .get<any>(this.URL_GET_LIBRO,
+        { observe: 'response' })
+      .pipe();
+  }
+
+  public AddPrestamo(entidad): Observable<HttpResponse<any>> {
+
+    return this.http
+      .post<any>(this.URL_ADD_PRESTAMO, entidad,
+        { observe: 'response' })
+      .pipe();
+  }
+
+  public UpdatePrestamo(entidad): Observable<HttpResponse<any>> {
+
+    return this.http
+      .post<any>(this.URL_UPDATE_PRESTAMO, entidad,
+        { observe: 'response' })
+      .pipe();
+  }
+
+  public DeletePrestamo(item): Observable<HttpResponse<any>> {
+  
+    let params = new HttpParams();
+    params = params.set('id', item.id);
+
+    return this.http
+      .post<any>(this.URL_DELETE_PRESTAMO,  "", {params: params, observe: 'response' })
+      .pipe();
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { PrestamoService } from '../servicios-backend/prestamo/prestamo.service';
+import { HttpResponse } from '@angular/common/http';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +10,166 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  public listPrestamo = [];
+  public listUsuario = [];
+  public listLibro = [];
+  public idPrestamo = ""
+  public idUsuario = ""
+  public idLibro = ""
+  public fechaRetiro = ""
+  public fechaDevolucion = ""
+  public swGuardarCambios = false
+
+  constructor(public navCtrl: NavController,
+    private prestamoServices: PrestamoService) {
+    this.GetPrestamo();
+    this.GetUsuario();
+    this.GetLibro();
+  }
+
+  private GetPrestamo() {
+    this.prestamoServices.GetPrestamo().subscribe({
+      next: (response: HttpResponse<any>) => {
+        this.listPrestamo = response.body;
+        //console.log(this.listPrestamo)
+      },
+      error: (error: any) => {
+        console.log(error);
+      },
+      complete: () => {
+        console.log('complete - this.GetPrestamo()');
+      },
+    });
+  }
+
+  private GetUsuario() {
+    this.prestamoServices.GetUsuario().subscribe({
+      next: (response: HttpResponse<any>) => {
+        this.listUsuario = response.body;
+        //console.log(this.listUsuario)
+      },
+      error: (error: any) => {
+        console.log(error);
+      },
+      complete: () => {
+        console.log('complete - this.GetPrestamo()');
+      },
+    });
+  }
+
+  private GetLibro() {
+    this.prestamoServices.GetLibro().subscribe({
+      next: (response: HttpResponse<any>) => {
+        this.listLibro = response.body;
+        //console.log(this.listLibro)
+      },
+      error: (error: any) => {
+        console.log(error);
+      },
+      complete: () => {
+        console.log('complete - this.GetUsuario()');
+      },
+    });
+  }
+
+  public addPrestamo() {
+    if (this.idUsuario.length > 0 && this.idLibro.length > 0 && this.fechaRetiro.length > 0 && this.fechaDevolucion.length > 0) {
+      var entidad = {
+        idUsuario : this.idUsuario,
+        idLibro : this.idLibro,
+        fechaRetiro : this.fechaRetiro,
+        fechaDevolucion : this.fechaDevolucion
+      }
+      console.log(entidad)
+      this.prestamoServices.AddPrestamo(entidad).subscribe({
+        next: (response: HttpResponse<any>) => {
+          console.log(response.body)//1
+          if(response.body == 1){
+            alert("Se agrego el Prestamo con exito :)");
+            this.GetPrestamo();//Se actualize el listado
+            this.idUsuario = "";
+            this.idLibro = "";
+            this.fechaRetiro = "";
+            this.fechaDevolucion = "";
+          }else{
+            alert("Al agregar el Prestamo fallo exito :(");
+          }
+        },
+        error: (error: any) => {
+          console.log(error);
+        },
+        complete: () => {
+          console.log('complete - this.addPrestamo()');
+        },
+      });
+    }
+  }
+
+  public guardarCambios(){
+    this.swGuardarCambios = false;
+    if (this.idUsuario.length > 0 && this.idLibro.length > 0 && this.fechaRetiro.length > 0 && this.fechaDevolucion.length > 0) {
+      var entidad = {
+        id: this.idPrestamo,
+        idUsuario : this.idUsuario,
+        idLibro : this.idLibro,
+        fechaRetiro : this.fechaRetiro,
+        fechaDevolucion : this.fechaDevolucion
+      }
+      console.log(entidad)
+      this.prestamoServices.UpdatePrestamo(entidad).subscribe({
+        next: (response: HttpResponse<any>) => {
+          console.log(response.body)//1
+          if(response.body == 1){
+            alert("Se modifico el Prestamo con exito :)");
+            this.GetPrestamo();//Se actualize el listado
+            this.idPrestamo = "";
+            this.idUsuario = "";
+            this.idLibro = "";
+            this.fechaRetiro = "";
+            this.fechaDevolucion = "";
+          }else{
+            alert("Al modificar el Prestamo fallo exito :(");
+          }
+        },
+        error: (error: any) => {
+          console.log(error);
+        },
+        complete: () => {
+          console.log('complete - this.guardarCambios()');
+        },
+      });
+    }
+  }
+
+  public updatePrestamo(item){
+    console.log(item)
+    this.idPrestamo = item.id
+    this.idUsuario = item.idUsuario + ""
+    this.idLibro = item.idLibro + ""
+    this.fechaRetiro = item.fechaRetiro
+    this.fechaDevolucion = item.fechaDevolucion
+    this.swGuardarCambios = true;
+  }
+
+  public deletePrestamo(item){
+    console.log(item.id)
+    this.prestamoServices.DeletePrestamo(item).subscribe({
+      next: (response: HttpResponse<any>) => {
+        console.log(response.body)//1
+        if(response.body == 1){
+          alert("Se elimino el Prestamo con exito :)");
+          this.GetPrestamo();//Se actualize el listado
+        }else{
+          alert("Al eliminar el Prestamo fallo exito :(");
+        }
+      },
+      error: (error: any) => {
+        console.log(error);
+      },
+      complete: () => {
+        console.log('complete - this.GetPrestamo()');
+      },
+    });
+  }
 
 }
